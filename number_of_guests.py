1827ab854ee27f12d679cc2fca4f22ce3ce121f5
Blake Hodges <tbhodges@gmail.com>
2024-03-26 14:30:13 -0400

    add guest list feature

--- number_of_guests.py
+++ number_of_guests.py
@@ -2,10 +2,15 @@ name = ''
 while not name:
     print("Welcome! Please enter your name:")
     name = input()
-    question = f"Hello {name}. How many guests will you have tonight?"
-    print(question)
-number_of_guests = input()
+question = f"Hello {name}. How many guests will you have tonight?"
+print(question)
+number_of_guests = input("Number of guests: ")
 if number_of_guests:
-    message = f"We will be sure to have enough room for all {number_of_guests} guests!"
+    guest_list = "GUEST LIST:\n"
+    print("\nPlease enter the names of all your guests.")
+    for i in range(int(number_of_guests)):
+        guest = input(f"Guest {i + 1}: ")
+        guest_list += guest + "\n"
+    message = f"\nWe will be sure to have enough room for all {number_of_guests} guests! Here is your guest list:\n\n{guest_list}"
     print(message)
 print('Thank you!')
