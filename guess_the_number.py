0088e8a2a9c422926fa8cc0338d3d57a57cc1df6
Blake Hodges <tbhodges@gmail.com>
2024-03-28 14:20:21 -0400

    add higher or lower messages to number guessing game

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,17 +4,19 @@ print("I'm thinking of a number between 1 and 10.")
 print("Take a guess.")
 
 while True:
-    guess = int(input("Your guess: "))
-    if (guess == target):
+    user_input = input("Your guess: ")
+    if user_input == 'exit':
+        break
+    guess = int(user_input)
+    if (guess == 'exit'):
+        break
+    if guess == target:
         print("\nGood guess. You are correct!")
         break
-    else:
-        print("\nSorry. That isn't correct.")
-        print("Do you want to guess again?")
-        keep_playing = input("Press 'Enter' to continue. Type anything else and press 'Enter' to quit playing.\n")
-        if (keep_playing == ""):
-            continue
-        else:
-            break
+    elif guess > target:
+        print("\nYour guess was too high.")
+    elif guess < target:
+        print("\nYour guess was too low.")
+    print("Continue guessing or enter 'exit' to quit playing.")
 
 print("Thanks for playing!")
